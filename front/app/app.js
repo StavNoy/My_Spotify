e5b8a66187c58ff73a20258bf6f698bce797d5ce
Benjam <benjamin.renel@wanadoo.fr>
2019-02-02 22:25:07 +0100

    views album data en dur

--- front/app/app.js
+++ front/app/app.js
@@ -1,6 +1,6 @@
 // rooter.get('url/albums/:id')
 
-url = `api.url/reception.php?request=albums&start=random&limit=10`;
+// url = `api.url/reception.php?request=albums&start=random&limit=10`;
 
 // $.ajax({
 //     type: "GET",
@@ -26,8 +26,14 @@ app.config(($routeProvider) => {
 });
 
 
-
 // CONTROLLER
+
+
+// Header nav 
+app.controller('HeaderNavCtrl', function ($scope, $http) {
+
+
+});
 
 // Home page
 app.controller('HomeCtrl', function ($scope, $http) {
@@ -38,7 +44,11 @@ app.controller('HomeCtrl', function ($scope, $http) {
 // List de tout les album
 app.controller('AlbumsCtrl', function ($scope, $http) {
 
+    $scope.test = {
+        id: 'sahdgfhajlskd'
+    }
 
+    $scope.merde = 'ehhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhf';
 });
 
 // Zoom sur un album
