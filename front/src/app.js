0e19927176eb4d1a0ae0adf425793f93d92f399a
Benjam <benjamin.renel@wanadoo.fr>
2019-02-03 16:05:00 +0100

    modif front view artist listage albums

---
+++ front/src/app.js
@@ -0,0 +1,65 @@
+const app = angular.module('MySpotify', ['ngRoute']);
+
+
+// ROUTE
+app.config(($routeProvider) => {
+
+    $routeProvider
+        .when('/', {templateUrl: './views/home.html', controller: 'HomeCtrl'})
+        .when('/albums', {templateUrl: './views/albums.html', controller: 'AlbumsCtrl'})
+        .when('/album/:id', {templateUrl: './views/album.html', controller: 'AlbumCtrl'})
+        .when('/artist/:id', {templateUrl: './views/artist.html', controller: 'ArtistCtrl'})
+        .otherwise({redirectTo : '/'});
+});
+
+
+// CONTROLLER
+
+
+// Header nav 
+app.controller('HeaderNavCtrl', function ($scope, $http) {
+
+
+});
+
+// Home page
+app.controller('HomeCtrl', function ($scope, $http) {
+
+    $http({
+        method: 'GET',
+        url: `../back/reception.php?request=albums&start=random&limit=8`
+    }).then(function successCallback(res) {
+        $scope.albums = res.data.data;
+        console.log($scope.albums)
+    });
+});
+
+// List de tout les album
+app.controller('AlbumsCtrl', function ($scope, $http) {
+
+    
+});
+
+// Zoom sur un album
+app.controller('AlbumCtrl', function ($scope, $http, $routeParams) {
+
+    $http({
+        method: 'GET',
+        url: `../back/reception.php?request=albums&id=${$routeParams.id}`
+    }).then(function successCallback(res) {
+        $scope.album = res.data.data;
+        console.log(res.data.data);
+    }); 
+});
+
+// Zoom sur un artist
+app.controller('ArtistCtrl', function ($scope, $http, $routeParams) {
+    
+    $http({
+        method: 'GET',
+        url: `../back/reception.php?request=artistes&id=${$routeParams.id}`
+    }).then(function successCallback(res) {
+        $scope.artist = res.data.data;
+        console.log(res.data.data);
+    });
+});
