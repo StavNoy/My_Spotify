433cb31708f6571652ae16b2631b09217bc99851
benjamin <benjamin.renel@wanadoo.fr>
2019-02-03 03:47:30 +0300

    angular album controller and dynamics data for album/id

--- front/app/app.js
+++ front/app/app.js
@@ -21,7 +21,7 @@ app.config(($routeProvider) => {
     $routeProvider
         .when('/', {templateUrl: './views/home.html', controller: 'HomeCtrl'})
         .when('/albums', {templateUrl: './views/albums.html', controller: 'AlbumsCtrl'})
-        .when('/albums/:id', {templateUrl: './views/album.html', controller: 'AlbumCtrl'})
+        .when('/album/:id', {templateUrl: './views/album.html', controller: 'AlbumCtrl'})
         .otherwise({redirectTo : '/'});
 });
 
@@ -44,15 +44,18 @@ app.controller('HomeCtrl', function ($scope, $http) {
 // List de tout les album
 app.controller('AlbumsCtrl', function ($scope, $http) {
 
-    $scope.test = {
-        id: 'sahdgfhajlskd'
-    }
-
-    $scope.merde = 'ehhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhf';
+    
 });
 
 // Zoom sur un album
 app.controller('AlbumCtrl', function ($scope, $http, $routeParams) {
 
-    $scope.id = $routeParams.id;
+    const url = `../back/reception.php?request=albums&id=${$routeParams.id}`;
+
+    $http({
+        method: 'GET',
+        url: url
+    }).then(function successCallback(res) {
+        $scope.album = res.data.data;
+    });
 });
